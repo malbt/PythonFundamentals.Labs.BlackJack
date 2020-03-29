56d497bf55ca566751f485449ba5e43af3b4afa7
mtessema <mal.bt27@gmail.com>
2020-03-29 17:30:43 -0400

    changed the overall function

--- black_jack.py
+++ black_jack.py
@@ -1,14 +1,48 @@
 import random
 
+
+def result():
+    if sum(dealer_card) < sum(player_card) <= 21:
+        print("player wins")
+        return
+    elif sum(dealer_card) > sum(player_card) <= 21:
+        print("dealer wins")
+        return
+    elif sum(dealer_card) == sum(player_card) <= 21:
+        print("it's a tie")
+        return
+    elif sum(player_card) > 21 and sum(dealer_card) <= 21:
+        print("dealer wins")
+        return
+    else:
+        print("game over")
+        return
+
+
+def option():
+    choice = input("choice-> HIT ,  STAY:")
+    choice = choice.upper()
+    print(choice)
+    return choice
+
+
 dealer_card = []
 
 
-def dealers_cards():
+def dealers_card():
     while len(dealer_card) != 2:
         dealer_card.append(random.randint(1, 11))
         if len(dealer_card) == 2:
+            print("dealer_card:hidden card & ", dealer_card[1])
             # print("hidden card & ", dealer_card[1])
-            print(dealer_card, sum(dealer_card))
+
+        elif len(dealer_card) == 2 and option() != "HIT":
+            print("dealer_card:", dealer_card, sum(dealer_card))
+
+            # print(dealer_card, sum(dealer_card))
+            return
+        elif len(dealer_card) == 2 and option() == "HIT":
+            print("dealer_card:", dealer_card, sum(dealer_card))
             return
 
 
@@ -16,50 +50,35 @@ player_card = []
 
 
 def players_card():
-    while len(player_card) != 2:
+    while len(player_card) != 2 or len(player_card) > 2:
         player_card.append(random.randint(1, 11))
         if len(player_card) == 2:
-            print(player_card, sum(player_card))
+            print("players card: ", player_card, sum(player_card))
             return
 
-
-def option():
-    choice = input("choice-> HIT ,  FOLD:")
-    choice = choice.upper()
-    print(choice)
-    return choice
+        elif len(player_card) == 2 and option() == "HIT":
+            player_card.append(random.randint(1, 11))
+            print("players card: ", player_card, sum(player_card))
+            return
 
 
 def game():
     if option() == "HIT":
         player_card.append(random.randint(1, 11))
-        print("players card:", player_card)
-        dealer_card.append(random.randint(1, 11))
-        print("dealers_card:", dealer_card)
-        result()
+        print("players card: ", player_card, sum(player_card))
+        print("dealer_card:", dealer_card, sum(dealer_card))
         return
     elif option != "HIT":
-        result()
-        return
-
-
-def result():
-    if sum(dealer_card) < sum(player_card) <= 21:
-        print("player wins")
-        return
-    elif sum(player_card) < sum(dealer_card) <= 21:
-        print("dealer wins")
-        return
-    elif sum(dealer_card) == sum(player_card) <= 21:
-        print("it's a tie")
-        return
-    else:
-        print("game over")
+        player_card.append(random.randint(1, 11))
+        print("players card: ", player_card, sum(player_card))
+        print("dealer_card:", dealer_card, sum(dealer_card))
         return
 
 
 if __name__ == '__main__':
     print("lets get started >>>")
+
 players_card()
-dealers_cards()
+dealers_card()
 game()
+result()
