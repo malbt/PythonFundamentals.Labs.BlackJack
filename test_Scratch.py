8baf3efb26869c56d0bf356e63e9e852aef98df1
mtessema <mal.bt27@gmail.com>
2020-03-28 22:52:52 -0400

    ideas

---
+++ test_Scratch.py
@@ -0,0 +1,20 @@
+import unittest
+import Scratch
+
+# class Casino:
+#     def __init__(self, persons: str):
+#         self.name = persons
+#         self.card = 0
+#         self.count = 0
+#
+#     def __repr__(self):
+#         return f'{self.name}'
+#
+#
+# class Zol:
+#     pass
+#
+#     def add_person(self, person) -> None:
+#         self.person = Casino(person)
+#
+Class TestScratch(unittest.TestCase)
