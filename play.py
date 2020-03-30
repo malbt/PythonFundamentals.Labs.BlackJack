1b0e04c2319c7845085795242b042b50eae54c01
mtessema <mal.bt27@gmail.com>
2020-03-29 20:24:35 -0400

    modules

---
+++ play.py
@@ -0,0 +1,10 @@
+from black_jack import players_card, dealers_card, game, result, option
+
+if __name__ == '__main__':
+    print("lets get started >>>")
+
+players_card()
+dealers_card()
+game()
+result()
+option()
