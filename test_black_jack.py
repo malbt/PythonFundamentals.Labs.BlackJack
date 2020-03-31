1b9afb670bb2bb89956a11eb95b3e7668aea2636
Maleda Tessema <mal.bt27@gmail.com>
2020-03-31 00:33:08 -0400

    added raise error to option()

--- test_black_jack.py
+++ test_black_jack.py
@@ -1,13 +1,14 @@
 import unittest
 from typing import Callable
 from unittest.mock import patch
+
+from nose.plugins.builtin import builtins
+
 from black_jack import option
 import black_jack
 import random
-import random.randint(1, 11)
 
 
-# noinspection PyShadowingNames
 class TestBlackJack(unittest.TestCase):
 
     def test_result(self):
@@ -19,6 +20,7 @@ class TestBlackJack(unittest.TestCase):
             black_jack.result = {"dealer wins"}
         if sum(black_jack.player_card) > 21 and sum(black_jack.dealer_card) <= 21:
             black_jack.result = {"dealer wins"}
+
     #
     # val = "hit"
     #
@@ -32,16 +34,33 @@ class TestBlackJack(unittest.TestCase):
     # def test_option(self, mock_input):
     #     result = black_jack.option()
     #     self.assertEqual("HIT", result)
-    val1 = []
+    # val1 = []
+
     # while len(dealer_card) != 2:
     #     dealer_card.append(random.randint(1, 11))
-    def test_dealers_card(self, append=None, val=None, val1=None):
-        while val == val1:
-            # result = val1(append.randint(1, 11))
-            # self.assertEqual(result, append.randint(1, 11))
-            if len(val1) == 2:
-                black_jack.dealers_card = {"dealer_card:hidden card &", val1[1]}
-            elif len(val1) == 2 and black_jack.option() != "HIT":
-                black_jack.dealers_card = {"dealer_card:", val1, sum(val1)}
-            elif len(val1) == 2 and black_jack.option() == "HIT":
-                black_jack.dealers_card = {"dealer_card:", val1, sum(val1)}
+    #
+    # @patch('random.randint')
+    # @patch(black_jack.dealer_card.append.mock_randint)
+    # def test_dealers_card(self):
+    #     while black_jack.dealer_card != 2:
+    #         val = []
+    #         result = val
+    #         self.assertEqual(result, val)
+    #         if len(black_jack.dealer_card) == 2:
+    #             black_jack.dealers_card = {"dealer_card:hidden card &", black_jack.dealer_card[1]}
+    #         elif len(black_jack.dealer_card) == 2 and black_jack.option() != "HIT":
+    #             black_jack.dealers_card = {"dealer_card:", black_jack.dealer_card, sum(black_jack.dealer_card)}
+    #         elif len(black_jack.dealer_card) == 2 and black_jack.option() == "HIT":
+    #             black_jack.dealers_card = {"dealer_card:", black_jack.dealer_card, sum(black_jack.dealer_card)}
+
+    # def option():
+    #     choice = input("choice-> HIT ,  STAY:").upper()
+    #     # choice = choice.upper()
+    #     print(choice)
+    #     return
+    val = "hit"
+
+    @patch('builtins.input', return_value=val)
+    def test_option():
+        result = black_jack.option()
+        builtins.input.assertEqual(result, "HIT")
